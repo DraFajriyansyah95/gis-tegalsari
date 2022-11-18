6c422a5669493984eef9c05a97557ab35d17b705
DraFajriyansyah95 <dradiamond1314@gmail.com>
2022-11-18 15:53:22 +0700

    menambahkan konten struktur organisasi

--- pages/struktur-organisasi.php
+++ pages/struktur-organisasi.php
@@ -16,64 +16,77 @@
             <div class="col-md-12">
                 <div class="tree">
                     <ul>
-                        <li>
-                            <a href="#">Kepala Desa : Hendi Kiswanto</a>
+                      <li class="justify-content-sm-center">
+                          <a href="#"><b>Kepala Desa</b>
+                            <p>Hendi Kiswanto</p></a>
                             <ul>
-                                <li>
-                                    <a href="#">Child</a>
-                                    <ul>
-                                        <li>
-                                            <a href="#">Grand Child</a>
-                                        </li>
+                              <li class="col-sm-4" >
+                                <a href="#"><b>Sekertaris Desa</b>
+                                  <p class="py-2">Vicky Dairi Adiansyah</p></a>
+                                    <ul class="container-sm-2 d-flex">
+                                      <li>
+                                      <a href="#"><b>Kepala Urusan Keuangan</b><p class="py-2">Dani Nurdiansah S.Pd.</p></a>
+                                      </li>
+                                      <li>
+                                        <a href="#"><b>Kepala Urusan Umum</b><p class="py-2">Tono</p></a>
+                                      </li>
+                                      <li>
+                                        <a href="#"><b>Kepala Urusan Perencanaan</b><p class="py-2">Zony Mardiansyah</p></a>
+                                      </li>
+                                    </ul>  
+                              </li>
+                              <li class="col-sm-4">
+                                  <a href="#"><b>Kepala Seksi</b></a>
+                                    <ul class="container-sm-2 d-flex">
+                                      <li>
+                                        <a href="#"><b>Pemerintahan</b><p class="py-2">Andrianus Maulana</p></a>
+                                      </li>
+                                      <li>
+                                        <a href="#"><b>Pelayanan</b><p class="py-2">Tisnaeni Sasmita S.Pd.</p></a>
+                                      </li>
+                                      <li>
+                                        <a href="#"><b>Kesejahteraan</b><p class="py-2">Cecep Effendi</p></a>
+                                      </li>
                                     </ul>
-                                </li>
-                                <li>
-                                    <a href="#">Child</a>
-                                    <ul>
-                                        <li><a href="#">Grand Child</a></li>
-                                        <li>
-                                            <a href="#">Grand Child</a>
-                                            <ul>
-                                                <li>
-                                                    <a href="#">Great Grand Child</a>
-                                                </li>
-                                                <li>
-                                                    <a href="#">Great Grand Child</a>
-                                                </li>
-                                                <li>
-                                                    <a href="#">Great Grand Child</a>
-                                                </li>
-                                            </ul>
-                                        </li>
-                                        <li><a href="#">Grand Child</a></li>
+                              </li>
+                              <li class="col-sm-4">
+                                <a href="#"><b>Kepala Dusun</b></a>
+                                    <ul class="container-sm-2 d-flex">                                      
+                                      <li>
+                                      <a href="#"><b>Karangsari</b><p class="py-2">Dede Slamet Nugraha</p></a>
+                                      </li>
+                                      <li>
+                                      <a href="#"><b>Mekarsari</b><p class="py-2">Diding Bahrudin</p></a>
+                                      </li>
+                                      <li>
+                                      <a href="#"><b>Mulyasari</b><p class="py-2">Dedih</p></a>
+                                      </li>
                                     </ul>
-                                </li>
+                              </li>
                             </ul>
                         </li>
                     </ul>
                 </div>
             </div>
         </div>
-        <div class="row">
-          <h1 class="level-1 rectangle">CEO</h1>
+
+        <!-- <div class="row">
+          <h1 class="level-1 rectangle">Hendi Kiswanto</h1>
           <ol class="level-2-wrapper">
             <li>
-              <h2 class="level-2 rectangle">Director A</h2>
+              <h2 class="level-2 rectangle">Vicky Dairi Adiansyah</h2>
               <ol class="level-3-wrapper">
                 <li>
                   <h3 class="level-3 rectangle">Manager A</h3>
                   <ol class="level-4-wrapper">
                     <li>
-                      <h4 class="level-4 rectangle">Person A</h4>
+                      <h4 class="level-4 rectangle">Dani Nurdiansah, S.Pd.</h4>
                     </li>
                     <li>
-                      <h4 class="level-4 rectangle">Person B</h4>
+                      <h4 class="level-4 rectangle">Tono</h4>
                     </li>
                     <li>
-                      <h4 class="level-4 rectangle">Person C</h4>
-                    </li>
-                    <li>
-                      <h4 class="level-4 rectangle">Person D</h4>
+                      <h4 class="level-4 rectangle">Zony Mardiansyah</h4>
                     </li>
                   </ol>
                 </li>
@@ -136,7 +149,7 @@
               </ol>
             </li>
           </ol>
-        </div>
+        </div> -->
     </div>
 <?php include "../components/footer.php"; ?>
 
